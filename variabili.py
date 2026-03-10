1e13c4c4606f4af32b14103fa882d869aee62d68
PAolo Rampino <liadserv@gmail.com>
2026-03-10 05:46:15 -0400

    UP

---
+++ variabili.py
@@ -0,0 +1,16 @@
+# iniziano per lettera a-z A-Z oppure _
+anonimo=0
+a_non=1
+_non=1
+# 1non=1 <-- lo posso scrivere perche' i numeri non possono trovarsi ad inizio variabile
+Anonimo=1 # con la maiuscola e' diverso da anonimo=0
+Anonimo2=4 # i numeri non posso essere messi per primi
+
+# non possono aver nomi di comando
+# if=0 <- questo non lo posso usare perche' if e' un comando
+# while=0 <- come sopra 
+
+ComeVa=89 # non e' sbagliato ma per convenzione viene utilizzato per le classi
+comeVa=40 # non e' sbagliato ma per convenzione viene utilizzato nei metodi
+come_va=40 # la variabile per convenzione va dichiarata in questo modo
+
