af61653ab01dbe1e32282a5ca34d7e7b1b1e8439
PAolo Rampino <liadserv@gmail.com>
2026-03-10 07:06:17 -0400

    UP

--- if_controllo_di_flusso.py
+++ if_controllo_di_flusso.py
@@ -4,7 +4,9 @@ y=int(input("Dammi il valore di y: "))
 
 print("la somma di",x, "e di", y, "e'", x+y)
 
-if x < y:
+if x<y:
     print("x e' minore di y")
+elif (x == y): # le parantesi sono opzionale per un singolo controllo
+    print("x e' uguale a y")
 else:
     print("x e' maggiore di y")
