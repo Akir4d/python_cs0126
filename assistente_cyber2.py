77689a6f8a801e62d87b85d9f023b488adbebab1
Paolo Rampino <liadserv@gmail.com>
2026-03-12 08:03:44 -0400

    UP

---
+++ assistente_cyber2.py
@@ -0,0 +1,39 @@
+from google import genai
+from google.genai import types
+import os
+
+# La nostra chiave non deve essere mai scritta nel codice
+chiave = os.environ["GEMINI_API_KEY"]
+# Instanzio un client genai e lo configuro con la chiave 
+client = genai.Client(api_key=chiave)
+
+config = types.GenerateContentConfig(
+    temperature=0.2,
+    max_output_tokens=1024,
+    system_instruction="""
+    Sei un tutor di sicurezza informatica per studenti principianti.
+    Spiega i concetti in modo semplice, con esempi pratici.
+    Se non sai qualcosa, dillo chiaramente.
+    """
+)
+contesto = ""
+while True:
+    # Qui faccio una domanda
+    domanda=input("Tu: ")
+    contesto += "user: " + domanda
+    # mi occorre una via d'uscita!
+    if domanda.lower().strip() == "esci":
+        print("Arrivederci!")
+        break
+    
+    # Genera risposta
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", # chiedo a google di usare il modello veloce
+        contents=contesto, # faccio una domanda al modello
+        config=config
+    )
+
+    contesto += "Assistant: " + response.text
+
+    # Stampa output
+    print(f"AI: {response.text}\n")
