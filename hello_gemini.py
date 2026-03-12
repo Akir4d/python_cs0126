31279b40e9fd2a219fd3c0f0bea59250bb9aeab7
Paolo Rampino <liadserv@gmail.com>
2026-03-12 07:25:36 -0400

    UP

---
+++ hello_gemini.py
@@ -0,0 +1,24 @@
+from google import genai
+from google.genai import types
+import os
+
+# La nostra chiave non deve essere mai scritta nel codice
+chiave = os.environ["GEMINI_API_KEY"]
+# Instanzio un client genai e lo configuro con la chiave 
+client = genai.Client(api_key=chiave)
+
+config = types.GenerateContentConfig(
+    temperature=0.1,
+    max_output_tokens=1024,
+)
+
+
+# Genera risposta
+response = client.models.generate_content(
+    model="gemini-2.5-flash", # chiedo a google di usare il modello veloce
+    contents="Ciao! Chi sei?", # faccio una domanda al modello
+    config=config
+)
+
+# Stampa output
+print(response.text)
